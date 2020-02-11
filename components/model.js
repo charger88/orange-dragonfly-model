1928ad50c6044a8815cbec87e23f249e18b60611
Michael Kelner <mikhail.kelner@gmail.com>
2020-02-10 20:23:36 -0600

    Update/create

--- components/model.js
+++ components/model.js
@@ -4,6 +4,8 @@ const validate = require('orange-dragonfly-validator')
 
 class Model extends ORM.ActiveRecord {
 
+  static IGNORE_EXTRA_FIELDS = false
+
   /**
    * Returns schema for the model (Orange Dragonfly Validator format)
    * @return object
@@ -40,6 +42,22 @@ class Model extends ORM.ActiveRecord {
   }
 
   /**
+   * Returns list of fields restricted for create method
+   * @return {Array}
+   */
+  static get restricted_for_create () {
+    return ['id'].concat(this.special_fields)
+  }
+
+  /**
+   * Returns list of fields restricted for update method
+   * @return {Array}
+   */
+  static get restricted_for_update () {
+    return ['id'].concat(this.special_fields)
+  }
+
+  /**
    * Lookup method
    * @param data
    * @return {SelectQuery}
@@ -50,16 +68,66 @@ class Model extends ORM.ActiveRecord {
     const filtered_rules = {}
     for (let field of Object.keys(data)) {
       if (!rules.hasOwnProperty(field)) {
+        if (this.IGNORE_EXTRA_FIELDS) {
+          continue
+        }
         throw new Error(`Field "${field}" is not described for model ${this.name}`)
       }
       if (this.restricted_for_lookup.includes(field)) {
-        throw new Error(`Field "${field}" is restricted for model ${this.name}`)
+        throw new Error(`Field "${field}" is restricted for searching model ${this.name}`)
       }
       q.where(field, data[field])
       filtered_rules[field] = Array.isArray(data[field]) ? {'type': 'array', 'children': {'*': rules[field]}} : rules[field]
     }
     validate(filtered_rules, data)
     return q
+  }
+
+  /**
+   * Creates object
+   * @param data
+   * @return {Promise<ActiveRecord>}
+   */
+  static async create (data) {
+    const new_data = {}
+    for (let field of Object.keys(data)) {
+      if (!rules.hasOwnProperty(field)) {
+        if (this.IGNORE_EXTRA_FIELDS) {
+          continue
+        }
+        throw new Error(`Field "${field}" is not described for model ${this.name}`)
+      }
+      if (this.restricted_for_create.includes(field)) {
+        throw new Error(`Field "${field}" is restricted for creating model ${this.name}`)
+      }
+      new_data[field] = data[field]
+    }
+    return await (new this(new_data)).save()
+  }
+
+  /**
+   * Updates object
+   * @param data
+   * @return {Promise<ActiveRecord>}
+   */
+  async update (data) {
+    if (!this.id) {
+      throw new Error(`You can update saved object only`)
+    }
+    const new_data = {}
+    for (let field of Object.keys(data)) {
+      if (!rules.hasOwnProperty(field)) {
+        if (this.constructor.IGNORE_EXTRA_FIELDS) {
+          continue
+        }
+        throw new Error(`Field "${field}" is not described for model ${this.name}`)
+      }
+      if (this.constructor.restricted_for_update.includes(field)) {
+        throw new Error(`Field "${field}" is restricted for updating model ${this.name}`)
+      }
+      new_data[field] = data[field]
+    }
+    return await this.save(new_data)
   }
 
   async _preSave () {
