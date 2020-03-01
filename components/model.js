824d7a9fa92a0cb2469f1b9f5958f6d92e986d9c
Michael Kelner <mikhail.kelner@gmail.com>
2020-03-01 17:30:51 -0600

    Fix

--- components/model.js
+++ components/model.js
@@ -133,10 +133,10 @@ class Model extends ORM.ActiveRecord {
         if (this.constructor.IGNORE_EXTRA_FIELDS) {
           continue
         }
-        throw new Error(`Field "${field}" is not described for model ${this.name}`)
+        throw new Error(`Field "${field}" is not described for model ${this.constructor.name}`)
       }
       if (this.constructor.restricted_for_update.includes(field)) {
-        throw new Error(`Field "${field}" is restricted for updating model ${this.name}`)
+        throw new Error(`Field "${field}" is restricted for updating model ${this.constructor.name}`)
       }
       new_data[field] = data[field]
     }
